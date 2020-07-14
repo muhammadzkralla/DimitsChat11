ca4f7fdf09c54a128e6ecfdbd5ba91a41cadb45c
mohamed <muhammad.heshamyt@gmail.com>
2020-07-14 17:38:01 +0200

    Added Chat Fragment

--- app/src/main/java/com/dimits/dimitschat/ui/main/chats/ChatsFragment.java
+++ app/src/main/java/com/dimits/dimitschat/ui/main/chats/ChatsFragment.java
@@ -4,19 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.dimits.dimitschat.R;
+import com.dimits.dimitschat.adapter.UserAdapter;
+import com.dimits.dimitschat.model.UserModel;
+import com.dimits.dimitschat.ui.main.users.UsersViewModel;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class ChatsFragment extends Fragment {
+    private ChatsViewModel chatsViewModel;
+    private List<UserModel> userModelsList;
+    RecyclerView recyclerView;
+    UserAdapter adapter;
+    Unbinder unbinder;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root =  inflater.inflate(R.layout.chats_fragment, container, false);
+        recyclerView = (RecyclerView)root.findViewById(R.id.recycler_chat);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        unbinder = ButterKnife.bind(this,root);
+        chatsViewModel = ViewModelProviders.of(this).get(ChatsViewModel.class);
+
+        chatsViewModel.getMessageError().observe(getViewLifecycleOwner(), s
+                -> Toast.makeText(getContext(),""+s,Toast.LENGTH_SHORT).show());
+
+
+        chatsViewModel.getUserList().observe(getViewLifecycleOwner(), userModels -> {
+            adapter = new UserAdapter(getContext(),userModels);
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        });
+
         return root;
+
     }
 }
--- app/src/main/java/com/dimits/dimitschat/ui/main/chats/ChatsViewModel.java
+++ app/src/main/java/com/dimits/dimitschat/ui/main/chats/ChatsViewModel.java
@@ -1,4 +1,109 @@
 package com.dimits.dimitschat.ui.main.chats;
 
-public class ChatsViewModel {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.dimits.dimitschat.callback.IUserCallbackListener;
+import com.dimits.dimitschat.common.Common;
+import com.dimits.dimitschat.model.ChatModel;
+import com.dimits.dimitschat.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class ChatsViewModel extends ViewModel implements IUserCallbackListener {
+    private MutableLiveData<List<UserModel>> userMutableLiveData;
+    private MutableLiveData<String> messageError = new MutableLiveData<>();
+    private IUserCallbackListener iUserCallbackListener;
+    private List<String> usersIds;
+
+    public ChatsViewModel(){
+        iUserCallbackListener = this;
+    }
+
+    public MutableLiveData<List<UserModel>> getUserList() {
+        if(userMutableLiveData == null) {
+            userMutableLiveData = new MutableLiveData<>();
+            messageError = new MutableLiveData<>();
+            getChats();
+        }
+        return userMutableLiveData;
+    }
+
+    private void getChats() {
+        usersIds = new ArrayList<>();
+        DatabaseReference chats = FirebaseDatabase.getInstance().getReference("Chats");
+        chats.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                usersIds.clear();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    ChatModel chats = dataSnapshot1.getValue(ChatModel.class);
+                    if (chats.getReceiver().equals(Common.currentUser.getUid())){
+                        usersIds.add(chats.getSender());
+                    }
+                    if (chats.getSender().equals(Common.currentUser.getUid())){
+                        usersIds.add(chats.getReceiver());
+                    }
+                }
+
+                loadUsers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+    }
+
+    private void loadUsers() {
+        List<UserModel> tempList = new ArrayList<>();
+        HashSet<String> stringHashSet = new HashSet<>(usersIds);
+        DatabaseReference users = FirebaseDatabase.getInstance().getReference("Users");
+        users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tempList.clear();
+                for (DataSnapshot userSnapShot:dataSnapshot.getChildren()){
+                    UserModel userModel = userSnapShot.getValue(UserModel.class);
+                    for (String userId : stringHashSet){
+                        if (userModel.getUid().equals(userId)){
+                            tempList.add(userModel);
+                        }
+                    }
+                }
+                iUserCallbackListener.onUserLoadSuccess(tempList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                iUserCallbackListener.onUserLoadFailed(databaseError.getMessage());
+            }
+        });
+    }
+
+
+    public MutableLiveData<String> getMessageError() {
+        return messageError;
+    }
+
+
+    @Override
+    public void onUserLoadSuccess(List<UserModel> userModels) {
+        userMutableLiveData.setValue(userModels);
+    }
+
+    @Override
+    public void onUserLoadFailed(String message) {
+        messageError.setValue(message);
+    }
 }

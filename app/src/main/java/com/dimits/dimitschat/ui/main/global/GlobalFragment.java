b689bd7a658a9fb2bdaf76fdabec1621a644c415
markrizkalla <mark.rizkalla1@gmail.com>
2020-07-16 14:17:41 +0200

    global chat with img error

--- app/src/main/java/com/dimits/dimitschat/ui/main/global/GlobalFragment.java
+++ app/src/main/java/com/dimits/dimitschat/ui/main/global/GlobalFragment.java
@@ -1,21 +1,93 @@
 package com.dimits.dimitschat.ui.main.global;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.dimits.dimitschat.MessagesActivity;
 import com.dimits.dimitschat.R;
+import com.dimits.dimitschat.adapter.GlobalAdapter;
+import com.dimits.dimitschat.callback.IGlobalCallbackListener;
+import com.dimits.dimitschat.common.Common;
+import com.dimits.dimitschat.globalActivity;
+import com.dimits.dimitschat.model.GlobalChatModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 
-public class GlobalFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.Unbinder;
+
+public class GlobalFragment extends Fragment{
+    GlobalViewModel globalViewModel;
+    EditText editText;
+    ImageView sendText;
+    private Unbinder unbinder;
+    RecyclerView recyclerView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //globalViewModel = ViewModelProviders.of(this).get(GlobalViewModel.class);
         View root =  inflater.inflate(R.layout.fragment_global, container, false);
+
+        Intent global = new Intent(getActivity(), globalActivity.class);
+        startActivity(global);
+
+
+       /* recyclerView =(RecyclerView)root.findViewById(R.id.recycler_global_chat);
+        //recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        sendText = (ImageView)root.findViewById(R.id.send_btn);
+        sendText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText =(EditText)root.findViewById(R.id.edt_message);
+                Map<String, Object> globalChatModel = new HashMap<>();
+                globalChatModel.put("sender", Common.currentUser.getName());
+                globalChatModel.put("message", editText.getText().toString());
+                //submittofirebase((List<GlobalChatModel>) globalChatModel);
+
+            }
+        });
+
+        globalViewModel.getmessages().observe(this, new Observer<List<GlobalChatModel>>() {
+            @Override
+            public void onChanged(List<GlobalChatModel> globalChatModels) {
+                GlobalAdapter globalAdapter = new GlobalAdapter(globalChatModels, getContext());
+                recyclerView.setAdapter(globalAdapter);
+            }
+        });*/
+
         return root;
     }
+
+
+/*
+    private void submittofirebase(List<GlobalChatModel> globalChatModel) {
+        DatabaseReference chatRef = FirebaseDatabase.getInstance().getReference();
+        chatRef.child("GlobalChats").push().setValue(globalChatModel);
+    }*/
 }
--- app/src/main/java/com/dimits/dimitschat/ui/main/global/GlobalViewModel.java
+++ app/src/main/java/com/dimits/dimitschat/ui/main/global/GlobalViewModel.java
@@ -1,4 +1,73 @@
 package com.dimits.dimitschat.ui.main.global;
 
-public class GlobalViewModel {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.dimits.dimitschat.callback.IGlobalCallbackListener;
+import com.dimits.dimitschat.common.Common;
+import com.dimits.dimitschat.model.GlobalChatModel;
+import com.dimits.dimitschat.model.UserModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalViewModel extends ViewModel implements IGlobalCallbackListener{
+    private MutableLiveData<List<GlobalChatModel>> modelMutableLiveDataGlobal;
+    private MutableLiveData<String> messageError = new MutableLiveData<>();
+    private IGlobalCallbackListener iGlobalCallbackListener;
+
+    public GlobalViewModel(){
+       iGlobalCallbackListener = this;
+    }
+
+    public MutableLiveData<List<GlobalChatModel>> getmessages() {
+        if(modelMutableLiveDataGlobal == null) {
+            modelMutableLiveDataGlobal = new MutableLiveData<>();
+            messageError = new MutableLiveData<>();
+            loadmessages();
+        }
+        return modelMutableLiveDataGlobal;
+    }
+
+    private void loadmessages() {
+        List<GlobalChatModel> tempList = new ArrayList<>();
+        DatabaseReference users = FirebaseDatabase.getInstance().getReference("GlobalChats");
+        users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tempList.clear();
+                for (DataSnapshot messageSnapShot:dataSnapshot.getChildren()){
+                    GlobalChatModel userModel = messageSnapShot.getValue(GlobalChatModel.class);
+                    userModel.setSender(Common.currentUser.getName());
+                    userModel.setSender(Common.currentUser.getUid());
+                    tempList.add(userModel);
+                }
+                iGlobalCallbackListener.onUserLoadSuccess(tempList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                iGlobalCallbackListener.onUserLoadFailed(databaseError.getMessage());
+            }
+        });
+    }
+    public MutableLiveData<String> getMessageError() {
+        return messageError;
+    }
+
+    @Override
+    public void onUserLoadSuccess(List<GlobalChatModel> globalChatModels) {
+       modelMutableLiveDataGlobal.setValue(globalChatModels);
+    }
+
+    @Override
+    public void onUserLoadFailed(String message) {
+
+    }
 }

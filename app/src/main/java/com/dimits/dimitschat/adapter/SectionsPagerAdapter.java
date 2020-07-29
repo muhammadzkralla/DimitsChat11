65d4736bed658a79ff36548afe0d8a62b7dfffc9
mohamed <muhammad.heshamyt@gmail.com>
2020-07-29 18:02:18 +0200

    UI Changes

--- app/src/main/java/com/dimits/dimitschat/adapter/SectionsPagerAdapter.java
+++ app/src/main/java/com/dimits/dimitschat/adapter/SectionsPagerAdapter.java
@@ -53,7 +53,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
+        // Show 3 total pages.
         return 3;
     }
 }
